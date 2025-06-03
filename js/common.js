68162d45ad1d6fcf3a9a328cf9d735e011b06816
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2025-06-03 18:10:15 +0600

    donate now handler added

--- js/common.js
+++ js/common.js
@@ -1,1 +1,36 @@
-console.log("donate js")
+
+
+function allBtnHandler(id, value, navPrice, amount) {
+    document.getElementById(id).addEventListener("click", function () {
+        const donationAmountNoakhaliInput = document.getElementById(value).value;
+        const donationAmount = parseInt(donationAmountNoakhaliInput);
+        const navBdt = setParseIntValue(navPrice)
+        if (donationAmount !== "number" && donationAmount > 0) {
+            const noakhaliBdt = setParseIntValue(amount);
+            const updateNoakhaliBdt = noakhaliBdt + donationAmount;
+            const updateNavBdt = navBdt - donationAmount;
+
+            if (updateNavBdt < 0) {
+                alert("you have no amount");
+                return;
+            }
+
+            document.getElementById(navPrice).innerText = updateNavBdt;
+            document.getElementById(amount).innerText = updateNoakhaliBdt;
+            donationAmountNoakhaliInput.value = '';
+        }
+        else {
+            alert("please enter a valid number")
+        }
+    })
+}
+
+
+
+
+
+function setParseIntValue(id) {
+    const parseIntValue = document.getElementById(id).innerText;
+    const convertParseIntValue = parseInt(parseIntValue);
+    return convertParseIntValue;
+}
--- js/donate.js
+++ js/donate.js
@@ -1,1 +1,5 @@
-console.log("donate js")
+
+
+allBtnHandler("donate-btn-noakhali", "donation-amount-noakhali", "bdt", "noakhali-bdt");
+allBtnHandler("donate-btn-feni", "donation-amount-feni", "bdt", "feni-bdt");
+allBtnHandler("donate-btn-quota", "donation-amount-quota", "bdt", "quota-bdt");
